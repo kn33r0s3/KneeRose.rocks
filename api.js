c574f30aad3d4b1808fbae230d86c69b40a706a7
Knee Rose <lmfao.xd.lol.lmao@gmail.com>
2023-11-24 00:04:50 -0500

    proxy updated

--- api.js
+++ api.js
@@ -18,12 +18,12 @@ const app = express();
 //   optionsSuccessStatus: 204,
 // };
 // app.use(cors(corsOptions));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://kneerose.rocks");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "https://kneerose.rocks");
+//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+//   res.header("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
 
 function fixLength(len) {
   if (len === 0) return "";
