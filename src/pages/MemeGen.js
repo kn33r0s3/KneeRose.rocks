ac2a62df183c10f21d70b5876b26517af2393f54
Knee Rose <lmfao.xd.lol.lmao@gmail.com>
2023-11-27 15:08:57 -0500

    proxy updated

--- src/pages/MemeGen.js
+++ src/pages/MemeGen.js
@@ -29,6 +29,7 @@ export default function MemeGen() {
       mode: "cors",
       method: "GET",
       headers: {
+        "Access-Control-Allow-Origin": "*",
         "Content-Type": "application/json",
       },
     })
