2c4798525789a1b1e1db4df14b0ee2ec775d9e06
Knee Rose <68586176+kn33r0s3@users.noreply.github.com>
2023-11-27 14:08:28 -0500

    Update api.js

--- api.js
+++ api.js
@@ -13,7 +13,7 @@ const { YouTube } = require("popyt");
 const app = express();
 // CORS configuration
 const corsOptions = {
-  origin: "https://kneerose.rocks",
+  origin: "https://kneerose.rocks/",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
   optionsSuccessStatus: 204,
@@ -35,7 +35,7 @@ app.get("/", (req, res) => {
 
 app.get("/getsong", async (req, res) => {
   try {
-    res.header("Access-Control-Allow-Origin", "https://kneerose.rocks");
+    res.header("Access-Control-Allow-Origin", "https://kneerose.rocks/");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header(
       "Access-Control-Allow-Headers",
@@ -70,7 +70,7 @@ app.get("/play", async (req, res) => {
     const info = await ytdl.getInfo(videoUrl);
     const format = ytdl.chooseFormat(info.formats, { filter: "audioonly" });
     const audioDuration = info.videoDetails.lengthSeconds;
-    res.header("Access-Control-Allow-Origin", "https://kneerose.rocks");
+    res.header("Access-Control-Allow-Origin", "https://kneerose.rocks/");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header(
       "Access-Control-Allow-Headers",
@@ -149,7 +149,7 @@ app.get("/meme", async (req, res) => {
     memereddits[Math.floor(Math.random() * memereddits.length)];
   try {
     var data = await getRandomMeme(randomSubreddit);
-    res.header("Access-Control-Allow-Origin", "https://kneerose.rocks");
+    res.header("Access-Control-Allow-Origin", "https://kneerose.rocks/");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header(
       "Access-Control-Allow-Headers",
