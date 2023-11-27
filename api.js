970c9ef1008b5c10bdfc16ae1f6c85680e65365a
Knee Rose <lmfao.xd.lol.lmao@gmail.com>
2023-11-27 12:07:30 -0500

    proxy updated

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const ytdl = require("ytdl-core");
 const ffmpeg = require("fluent-ffmpeg");
 const ffmpegPath = require("ffmpeg-static");
