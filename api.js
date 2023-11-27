1ee9138bf758fed18b010f806e9ef164d321a3d1
Knee Rose <lmfao.xd.lol.lmao@gmail.com>
2023-11-27 13:21:45 -0500

    proxy updated

--- api.js
+++ api.js
@@ -19,7 +19,7 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://kneerose.rocks/");
+  res.header("Access-Control-Allow-Origin", "https://kneerose.rocks");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
