ae15acacb7e704dc05cf58900409007f7a85ad33
Knee Rose <lmfao.xd.lol.lmao@gmail.com>
2023-11-27 13:49:17 -0500

    proxy updated

--- src/pages/MemeGen.js
+++ src/pages/MemeGen.js
@@ -26,7 +26,7 @@ export default function MemeGen() {
   useEffect(() => {}, [memes]);
   function getMeme() {
     fetch("https://api.kneerose.rocks/meme", {
-      mode: "no-cors",
+      mode: "cors",
       method: "GET",
       headers: {
         "Content-Type": "application/json",
