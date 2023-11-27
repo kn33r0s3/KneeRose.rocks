0ff4d446a4ccd32404978f83eb223db286281d2f
Knee Rose <lmfao.xd.lol.lmao@gmail.com>
2023-11-27 12:12:26 -0500

    proxy updated

--- api.js
+++ api.js
@@ -32,7 +32,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/getsong", async (req, res) => {
-  res.header("Access-Control-Allow-Origin", "https://www.kneerose.rocks");
+  res.header("Access-Control-Allow-Origin", "https://kneerose.rocks");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   try {
     const youTube = new YouTube("AIzaSyAStVZQT5LnJOl5V1wapnQzVAXbca56ILs");
@@ -57,7 +57,7 @@ app.get("/getsong", async (req, res) => {
 });
 
 app.get("/play", async (req, res) => {
-  res.header("Access-Control-Allow-Origin", "https://www.kneerose.rocks");
+  res.header("Access-Control-Allow-Origin", "https://kneerose.rocks");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   const videoUrl = req.query.url;
 
@@ -82,7 +82,7 @@ app.get("/play", async (req, res) => {
 });
 
 app.get("/meme", async (req, res) => {
-  res.header("Access-Control-Allow-Origin", "https://www.kneerose.rocks");
+  res.header("Access-Control-Allow-Origin", "https://kneerose.rocks");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   const { getRandomMeme } = require("@blad3mak3r/reddit-memes");
 
