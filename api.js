52007ebd716ef29d4702eb5ae364b44d2c7b58cb
Knee Rose <lmfao.xd.lol.lmao@gmail.com>
2023-11-27 13:01:05 -0500

    proxy updated

--- api.js
+++ api.js
@@ -20,7 +20,8 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://kneerose.rocks");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
