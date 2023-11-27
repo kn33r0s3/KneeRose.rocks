3401694f86f62843fa1c2d74a9def3dcc062a722
Knee Rose <lmfao.xd.lol.lmao@gmail.com>
2023-11-27 12:22:17 -0500

    proxy updated

--- src/pages/MemeGen.js
+++ src/pages/MemeGen.js
@@ -29,7 +29,7 @@ export default function MemeGen() {
       mode: "cors",
       method: "GET",
       headers: {
-        "Access-Control-Allow-Origin": "https://www.kneerose.rocks",
+        "Access-Control-Allow-Origin": "https://kneerose.rocks",
         "Content-Type": "application/json",
       },
     })
