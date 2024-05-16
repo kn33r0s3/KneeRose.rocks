7524b67af63c354d47d309b0a15ba9ce0eddf0ca
Knee Rose <lmfao.xd.lol.lmao@gmail.com>
2024-05-15 21:22:12 -0400

    first commit

--- src/components/music/Player.js
+++ src/components/music/Player.js
@@ -75,9 +75,7 @@ export default function Player(props) {
 
   useEffect(() => {
     fetch(`https://www.api.kneerose.rocks${audioUrl}`, {
-      mode: "no-cors",
       method: "GET",
-      headers: { Range: "bytes=0-" },
     })
       .then((response) => response.blob())
       .then((blob) => {
